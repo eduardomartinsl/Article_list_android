5c8f423df6b0b9706f5141c7b8cc10596a10e2b9
Eduardo M <emartinslemos@gmail.com>
2020-12-04 16:21:53 -0400

    initial layout squeleton

---
+++ app/src/main/java/com/martins/article_list/ArticlesListActivity.kt
@@ -0,0 +1,11 @@
+package com.martins.article_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticlesListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_articles_list)
+    }
+}
